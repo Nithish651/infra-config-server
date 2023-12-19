3cb9d07f6036e3135638996f0e5cb784719af213
Nithish651 <nithish651@gmail.com>
2023-12-19 20:24:39 +0530

    Initial commit

---
+++ config-server/src/test/java/com/financetracker/configserver/ConfigServerApplicationTests.java
@@ -0,0 +1,13 @@
+package com.financetracker.configserver;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class ConfigServerApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
